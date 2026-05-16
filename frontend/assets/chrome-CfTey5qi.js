81f904972ef32f0b609541b1541821e3ad8bd91c
HaierKeys <haierkeys@gmail.com>
2026-05-16 17:19:57 +0800

    ### ✨ 新功能 - **版本渠道选择**：支持在“正式版”与“测试版”之间切换更新源。 - **令牌连接客户端**：授权令牌管理器现在可以展示已连接客户端的最近活跃记录。 - **客户端踢出功能**：客户端管理支持强制踢出某一个WebSocket连接的客户端。 ### 💄 其他/体验 - **支持名单UI调整**：优化相关显示。

---
+++ frontend/assets/chrome-CfTey5qi.js
@@ -0,0 +1,13 @@
+import{c as y}from"./font-loader-BOepJRqw.js";
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=y("Apple",[["path",{d:"M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z",key:"3s7exb"}],["path",{d:"M10 2c1 .5 2 2 2 5",key:"fcco2y"}]]),c=y("Chrome",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"4",key:"4exip2"}],["line",{x1:"21.17",x2:"12",y1:"8",y2:"8",key:"a0cw5f"}],["line",{x1:"3.95",x2:"8.54",y1:"6.06",y2:"14",key:"1kftof"}],["line",{x1:"10.88",x2:"15.46",y1:"21.94",y2:"14",key:"1ymyh8"}]]);
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */export{e as A,c as C};
---
+++ frontend/assets/index-B6TycXEv.js
@@ -0,0 +1,7 @@
+import{c as e,r}from"./font-loader-BOepJRqw.js";
+/**
+ * @license lucide-react v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const u=e("Clock",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]]);function o(e){const u=r.useRef({value:e,previous:e});return r.useMemo(()=>(u.current.value!==e&&(u.current.previous=u.current.value,u.current.value=e),u.current.previous),[e])}export{u as C,o as u};
